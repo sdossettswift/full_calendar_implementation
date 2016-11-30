676da485444d5a41461f74d7a43ea1298c8f8073
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-11-30 16:50:51 -0600

    full calendar renders

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,15 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require moment 
+//= require fullcalendar
+
+$(document).ready(function() {
+
+    // page is now ready, initialize the calendar...
+
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+    })
+
+});
